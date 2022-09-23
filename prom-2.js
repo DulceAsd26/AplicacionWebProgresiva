b5c4223c37e31bdab60116f4be2c713aa6b8f210
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-09-22 20:47:22 -0500

    Manejo de errores en las promesas

--- prom-2.js
+++ prom-2.js
@@ -4,6 +4,11 @@ function sumarUno( numero){
 
     var promesa = new Promise( function(resolve, reject){
 
+        console.log(numero);
+        if (numero >= 7) {
+            reject('El numero es muy alto');
+        }
+
     setTimeout( function(){
 
        resolve( numero + 1);
@@ -17,16 +22,16 @@ function sumarUno( numero){
 }
 
 
-sumarUno( 5 ).then( nuevoNumero => {
-    console.log(nuevoNumero);
-    return sumarUno( nuevoNumero);
-})
-.then( nuevoNumero => {
-    console.log(nuevoNumero);
-    return sumarUno( nuevoNumero);
-})
-.then( nuevoNumero => {
+sumarUno( 8 )
+   .then( sumarUno )
+   .then( sumarUno )
+   .then( sumarUno )
+   .then( nuevoNumero => {
     console.log(nuevoNumero);
 })
+.catch( error => {
+    console.log('ERROR DE PROMESA');
+    console.log(error);
+});
 
 
